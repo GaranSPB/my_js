ef230525ca027b9dd349bb50415f59f5da5a4192
GaranSPB <garan18pro@gmail.com>
2018-03-18 21:03:52 +0300

    full code refactoring, new usage, arguments

--- decorator.js
+++ decorator.js
@@ -1,29 +1,9 @@
 'use strict';
-var Decorator = function(target,property,filter,predicator){
-	try{
-		checkForConstructionErrors();
-		Object.defineProperty(this,'target',{value: target,writable: false});
-		Object.defineProperty(this,'property',{value: property,writable: false});
-		Object.defineProperty(this,'filter',{value: filter,writable: false});
-		Object.defineProperty(this,'predicator',{value: predicator,writable: false});
-	}catch(e){
-		console.error(e);		
-	}
-
-	function checkForConstructionErrors(){
-		if(target === undefined || !(target instanceof Object)) throw "Decorator construction error: target is not an Object!";
-		if(!target.hasOwnProperty(property)) throw "Decorator construction error: unknown property for target!";
-		if(filter === undefined || !(filter instanceof Function)) throw "Decorator construction error: 3rd param is not a function!";
-		if(predicator && !(predicator instanceof Function)) throw "Decorator construction error: 4rd param is not a function!";	
+var Decorator = function(property,filter,predicator){
+	return function(){
+		var target = this;
+		if(predicator === undefined || predicator.apply(target,arguments)){
+			target[property] instanceof Function ? target[property](filter.apply(target,arguments)) : target[property] = filter.apply(target,arguments);
+		}
 	}	
 }
-
-Decorator.prototype.set = function(value) {
-	if(this.predicator === undefined || this.predicator(value,this.target)){
-		var _prop = this.target[this.property];
-		_prop instanceof Function ? _prop(this.filter(value)) : _prop = this.filter(value);
-	}
-};
-
-
-
