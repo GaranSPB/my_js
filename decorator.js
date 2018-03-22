e5ba3ed6f1e9aac932fd1fa48511f02cdacd5604
GaranSPB <garan18pro@gmail.com>
2018-03-22 21:35:34 +0300

    prepared for setFilter

--- decorator.js
+++ decorator.js
@@ -19,8 +19,21 @@ var Decorator = function(name,target,property,filter,predicator){
 	_this.tartgets = [].concat(target);
 	if(!target.hasOwnProperty('decorators'))target.decorators = {};
 	target.decorators[name] = _this;
-	target[name] = decor(property,filter,predicator);
+	target[name] = _this.decor(target);
 }
+
+Decorator.prototype.decor = function(target){
+	var _this = this;
+	return function(){
+		var dec = target.decorators[_this.name],
+			predicator = dec.predicator,
+			filter = dec.filter,
+			property = dec.property;
+		if(predicator === undefined || predicator.apply(target,arguments)){
+			target[property] instanceof Function ? target[property](filter.apply(target,arguments)) : target[property] = filter.apply(target,arguments);
+		}
+	}
+} 
 
 Decorator.prototype.addTarget = function(target){
 	if(this.tartgets.indexOf(target) === -1){
