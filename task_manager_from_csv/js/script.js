5cb036c989c794c37cef98e8b2f4b6c9cc2e1a1f
Igor Garankov <37190014+GaranSPB@users.noreply.github.com>
2018-05-30 23:56:47 +0300

    Create script.js

---
+++ task_manager_from_csv/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	//var storage = new BowStorage(['csv/bow04.csv','csv/ebow04.csv','csv/bow05.csv','csv/ebow05.csv']);
+	// 'csv/bow.csv','csv/ebow.csv' february
+	// document.addEventListener(storage.readyEvent.type,function(){
+		// var	ws = new WorkSpace(storage);
+        // ws.createMyBowList().createFullBowList();
+        // console.log(ws);
+	// });
+});
